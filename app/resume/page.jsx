79dbfebfd062e6a1c946a408b51aa8f9ac434c7b
Karan Gupta <baniyakaran000@gmail.com>
2024-08-04 15:01:21 +0600

    added contact

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -227,33 +227,34 @@ const Resume = () => {
             </TabsContent>
 
             {/* skills */}
-            <TabsContent value="skills" className="w-full h-full">
-              <div className='flex flex-col gap-[30px]'>
-                <div className='flex flex-col gap-[30px] text-center xl:center xl:text-left'>
-                  <h3 className='text-4xl font-bold'>{skills.title}</h3>
-                  <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>
+            <TabsContent 
+            value="skills" 
+            className="w-full h-full"
+            >
+              <div className="flex flex-col gap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
                     {skills.description}
                   </p>
                 </div>
-                <ul className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]'>
-                  {skills.skillList.map((skill, index) => {
-                    return (
-                      <li key={index} className='bg-[#232329] h-[160px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1'>
-                        <TooltipProvider delayDuration={100}>
-                          <Tooltip>
-                            <TooltipTrigger className='w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group'>
-                              <div className='text-6xl group-hover:text-accent transition-all duration-300'>
-                                {skill.icon}
-                              </div>
-                            </TooltipTrigger>
-                            <TooltipContent>
-                              <p className='capitalize'>{skill.name}</p>
-                            </TooltipContent>
-                          </Tooltip>
-                        </TooltipProvider>
-                      </li>
-                    );
-                  })}
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
+                  {skills.skillList.map((skill, index) => (
+                    <li key={index} className="bg-[#232329] h-[160px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                      <TooltipProvider delayDuration={100}>
+                        <Tooltip>
+                          <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                            <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                              {skill.icon}
+                            </div>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p className="capitalize">{skill.name}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </li>
+                  ))}
                 </ul>
               </div>
             </TabsContent>
@@ -261,21 +262,23 @@ const Resume = () => {
             {/* about */}
             <TabsContent
               value="about"
-              className="w-full text-center xl:text-left"
+              className="w-full h-[510px] mb-[70px]"
             >
               <div className="flex flex-col gap-[30px]">
-                <h3 className="text-4xl font-bold">{about.title}</h3>
-                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
-                  {about.description}
-                </p>
-                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{about.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                    {about.description}
+                  </p>
+                </div>
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-8 max-w-[620px] mx-auto xl:mx-0">
                   {about.info.map((item, index) => (
                     <li
                       key={index}
                       className="flex items-center justify-center xl:justify-start gap-4"
                     >
                       <span className="text-white/60">{item.fieldName}</span>
-                      <span className="text-xl">{item.fieldValue}</span>
+                      <span className="text-xl hover:text-accent">{item.fieldValue}</span>
                     </li>
                   ))}
                 </ul>
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,27 +1,41 @@
 import Link from "next/link";
-import {FaGithub, FaLinkedinIn, FaInstagram} from "react-icons/fa";
+import { FaGithub, FaLinkedinIn, FaInstagram } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
-
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const socials = [
-    {icon: <FaGithub/>, path:"https://github.com/karanG69"},
-    {icon: <FaLinkedinIn/>, path:"https://www.linkedin.com/in/karan-gupta-2716a0213/"},
-    {icon: <SiLeetcode />, path:"https://leetcode.com/u/baniyakaran/"},
-    {icon: <FaInstagram/>, path:"https://www.instagram.com/__karan_g__/"},
-]
+    { icon: <FaGithub />, path: "https://github.com/karanG69", name: "GitHub" },
+    { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/karan-gupta-2716a0213/", name: "LinkedIn" },
+    { icon: <SiLeetcode />, path: "https://leetcode.com/u/baniyakaran/", name: "LeetCode" },
+    { icon: <FaInstagram />, path: "https://www.instagram.com/__karan_g__/", name: "Instagram" },
+];
 
-const Socials = ({containerStyles, iconStyles}) => {
-  return (
-    <div className={containerStyles}>
-        {socials.map((item, index)=> {
-            return ( 
-                <Link key={index} href={item.path} className={iconStyles}>
-                    {item.icon}
-                </Link>
-            );
-        })}
-    </div>
-  )
-}
+const Socials = ({ containerStyles, iconStyles }) => {
+    return (
+        <TooltipProvider>
+            <div className={containerStyles}>
+                {socials.map((item, index) => {
+                    return (
+                        <Tooltip key={index}>
+                            <TooltipTrigger asChild>
+                                <Link href={item.path} className={iconStyles}>
+                                    {item.icon}
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <span>{item.name}</span>
+                            </TooltipContent>
+                        </Tooltip>
+                    );
+                })}
+            </div>
+        </TooltipProvider>
+    );
+};
 
-export default Socials
+export default Socials;
